6c8a767571719925a4cf2ab79af67285b01ecf4f
GitHub Action <action@github.com>
2022-12-24 06:11:46 +0000

    Template cleanup

--- src/main/kotlin/com/github/chichi1091/packageinfovisualization/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/chichi1091/packageinfovisualization/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.chichi1091.packageinfovisualization.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.chichi1091.packageinfovisualization.MyBundle
 
 class MyApplicationService {
 
